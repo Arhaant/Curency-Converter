33437159d8975d35e8f6243e307a0ecfb943d3a3
Arhaant <anshuman.bhagowati@gmail.com>
2022-02-20 17:00:02 +0530

    Signed-off-by: Arhaant <anshuman.bhagowati@gmail.com>

--- App.js
+++ App.js
@@ -8,9 +8,27 @@ export default class App extends Component{
     super()
     this.state={
       textorsomething: '',
-
+      sankhya: '',
     }
   }
+
+  chaabikakarya=(emptyVariable)=>{
+    if(emptyVariable == 'see'){
+      this.setState({sankhya : ''})
+    }
+    else if(emptyVariable == 'bakcspaes'){
+     //str = 123
+     //str.length = 3
+     var str = this.state.sankhya
+     var overPowered = str.slice(0,str.length-1) 
+     this.setState({sankhya : overPowered})
+    }
+    else{
+    emptyVariable=this.state.sankhya + emptyVariable
+    this.setState({sankhya : emptyVariable})
+    }
+  }
+
   render(){
   return (
     <View style={styles.container}>
@@ -21,12 +39,12 @@ export default class App extends Component{
 
       <View style={styles.currencyMainContainer}>
         <View style={styles.currencySubContainer}>
-          <Pressable style={styles.currencyQuarterContainer1}>
-            <Text>Currency 1</Text>
+          <Pressable style={styles.currencyQuarterContainer}>
+            <Text style={styles.currencyTextile}>Currency 1</Text>
           </Pressable>
 
-          <Pressable style={styles.currencyQuarterContainer2}>
-            <Text>Currency to the right of 1</Text>
+          <Pressable style={styles.currencyQuarterContainer}>
+            <Text style={styles.currencyTextile}>Currency to the right of 1</Text>
           </Pressable>
         </View>
 
@@ -36,15 +54,15 @@ export default class App extends Component{
         <TextInput
           style={styles.currencyQuarterContainer3}
           onChangeText={(textorsomething)=>this.setState({textorsomething})}
-          //value={'1'}
-          placeholder="¯\_( ͡° ͜ʖ ͡°)_/¯"
-          keyboardType="numeric"
+          value={this.state.sankhya.toString()}
+          //placeholder="¯\_( ͡° ͜ʖ ͡°)_/¯"
+          //keyboardType="numeric"
         />
           <TextInput
           style={styles.currencyQuarterContainer4}
-          //value={'1'}
-          placeholder="¯\_(ツ)_/¯"
-          keyboardType="numeric"
+          //value={this.state.sankhya.toString()}
+          //placeholder="¯\_(ツ)_/¯"
+          //keyboardType="numeric"
         />
         </View>
 
@@ -53,46 +71,22 @@ export default class App extends Component{
       <View style={styles.chaabikaadda}>
         <View style={styles.chaabikaguchha}>
           <Pressable style={styles.chaabikasheily}>
-            <Text style={styles.chaabikapath}>1</Text>
+            <Text style={styles.chaabikapath}  onPress={()=>this.chaabikakarya('1')}>1</Text>
           </Pressable>
 
 
           <Pressable style={styles.chaabikasheily}>
-            <Text style={styles.chaabikapath}>2</Text>
+            <Text style={styles.chaabikapath}  onPress={()=>this.chaabikakarya('2')}>2</Text>
           </Pressable>
 
 
           <Pressable style={styles.chaabikasheily}>
-            <Text style={styles.chaabikapath}>3</Text>
+            <Text style={styles.chaabikapath}  onPress={()=>this.chaabikakarya('3')}>3</Text>
           </Pressable>
 
 
           <Pressable style={styles.chaabikasheily}>
-            <Text style={styles.chaabikapath}>←</Text>
-          </Pressable>
-        </View>
-
-
-
-
-        <View style={styles.chaabikaguchha}>
-          <Pressable style={styles.chaabikasheily}>
-            <Text style={styles.chaabikapath}>4</Text>
-          </Pressable>
-
-
-          <Pressable style={styles.chaabikasheily}>
-            <Text style={styles.chaabikapath}>5</Text>
-          </Pressable>
-
-
-          <Pressable style={styles.chaabikasheily}>
-            <Text style={styles.chaabikapath}>6</Text>
-          </Pressable>
-
-
-          <Pressable style={styles.chaabikasheily}>
-            <Text style={styles.chaabikapath}>C</Text>
+            <Text style={styles.chaabikapath}  onPress={()=>this.chaabikakarya('bakcspaes')}>←</Text>
           </Pressable>
         </View>
 
@@ -101,22 +95,46 @@ export default class App extends Component{
 
         <View style={styles.chaabikaguchha}>
           <Pressable style={styles.chaabikasheily}>
-            <Text style={styles.chaabikapath}>7</Text>
+            <Text style={styles.chaabikapath}  onPress={()=>this.chaabikakarya('4')}>4</Text>
           </Pressable>
 
 
           <Pressable style={styles.chaabikasheily}>
-            <Text style={styles.chaabikapath}>8</Text>
+            <Text style={styles.chaabikapath}  onPress={()=>this.chaabikakarya('5')}>5</Text>
           </Pressable>
 
 
           <Pressable style={styles.chaabikasheily}>
-            <Text style={styles.chaabikapath}>9</Text>
+            <Text style={styles.chaabikapath}  onPress={()=>this.chaabikakarya('6')}>6</Text>
           </Pressable>
 
 
           <Pressable style={styles.chaabikasheily}>
-            <Text style={styles.chaabikapath}>0</Text>
+            <Text style={styles.chaabikapath}  onPress={()=>this.chaabikakarya('see')}>C</Text>
+          </Pressable>
+        </View>
+
+
+
+
+        <View style={styles.chaabikaguchha}>
+          <Pressable style={styles.chaabikasheily}>
+            <Text style={styles.chaabikapath}  onPress={()=>this.chaabikakarya('7')}>7</Text>
+          </Pressable>
+
+
+          <Pressable style={styles.chaabikasheily}>
+            <Text style={styles.chaabikapath}  onPress={()=>this.chaabikakarya('8')}>8</Text>
+          </Pressable>
+
+
+          <Pressable style={styles.chaabikasheily}>
+            <Text style={styles.chaabikapath}  onPress={()=>this.chaabikakarya('9')}>9</Text>
+          </Pressable>
+
+
+          <Pressable style={styles.chaabikasheily}>
+            <Text style={styles.chaabikapath}  onPress={()=>this.chaabikakarya('0')}>0</Text>
           </Pressable>
         </View>
 
@@ -135,6 +153,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     
+  },
+  currencyTextile: {
+    //padding:
   },
   titleContainer: {
     alignItems: 'center',
